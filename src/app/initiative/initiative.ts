3aa3078e7eef6c78dbc50641f6b0b47192260353
Omar <info@omaralamoudi.net>
2021-01-06 13:31:50 +0300

    perf(initiative): Reconstruct

--- src/app/initiative/initiative.ts
+++ src/app/initiative/initiative.ts
@@ -1,20 +1,30 @@
-export class Initiative {
-  id: number;
-  category: InitiativeCategory;
-  tasks?: InitiativeTask[];
-  created: string;
-}
 export interface InitiativeCategory {
   id: number;
   title: string;
   description: string;
+  initiatives: Initiative[];
+  goals?: InitiativeGoal[];
+  created: string;
+}
+export class Initiative {
+  id: number;
+  title: string;
+  description: string;
+  tasks?: InitiativeTask[];
+  goals?: InitiativeGoal[];
   created: string;
 }
 export interface InitiativeTask {
   id: number;
-  task: string;
-  description: string;
+  name: string;
+  description?: string;
   done: boolean;
   created: string;
   modified: string;
+}
+export interface InitiativeGoal {
+  id: number;
+  name: string;
+  description?: string;
+  created: string;
 }
